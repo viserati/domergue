771e0ea67b20ce1081c1639f90978b94bf76f6c0
Keith Turner <turner@keithturner.com>
2018-08-28 07:35:01 -0600

    Controller clarificaion.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  # Controller for posts.
+  # Controller for posts..
   # GET /posts
   # GET /posts.json
   def index
